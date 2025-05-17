8f73016bc0a820259a9fdeb1c7f9f75b056058da
Roche Ngaleu <ibikivan1@gmail.com>
2025-05-18 00:38:31 +0200

    Ajout de tailwind et de daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + S3</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='p-6'>
+      <label className='floating-label my-4'>
+        <input type="text" name="type_here" id="type_here" placeholder='Tapez ici' className='input' />
+        <span className='label'>Tapez ici</span>
+      </label>
+
+      <button className='btn btn-primary'>C'est noté</button>
+    </div>
   )
 }
 
