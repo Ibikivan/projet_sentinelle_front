ef0f80d7476fc846942846836492bf6ed899a499
Ibikivan <82176594+Ibikivan@users.noreply.github.com>
2025-05-27 03:47:44 +0200

    Fix de déploiement et ajout du spinner

--- src/app/axios/axiosInstance.js
+++ src/app/axios/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = import.meta.env.API_URL || 'http://localhost:3000/api'
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
--- src/auth/AuthProvider.jsx
+++ src/auth/AuthProvider.jsx
@@ -1,15 +1,24 @@
 import { useAppStrore } from "../app/store"
 import { getCurrentUser } from "../utils/api/api"
 import { useQuery } from "react-query"
+import { toBoolean } from "../utils/helper"
 
 export default function AuthProvider({ children }) {
     const updateUser = useAppStrore.use.updateUser()
     const provideAuth = useAppStrore.use.provideAuth()
     const queryKey = ['currentUser']
+
+    let retry = import.meta.env.VITE_RETRY_LIMIT
+
+    if (retry === "false") {
+        retry = toBoolean(retry)
+    } else {
+        retry = parseInt(retry) || 2
+    }
     
     const { isLoading, data } = useQuery(queryKey, () => getCurrentUser(), {
         enabled: provideAuth,
-        retry: false
+        retry: retry || false,
     })
 
     if (data) updateUser(data)
@@ -18,7 +27,7 @@ export default function AuthProvider({ children }) {
         useAppStrore.persist.clearStorage()
     }
 
-    if (isLoading) return <h1>Loading...</h1>
+    if (isLoading) return <span className="relative h-screen icon-[line-md--loading-twotone-loop]"></span>
 
     return <div className="relative h-screen">
         {/* Potentiel background travaillé ici */}
