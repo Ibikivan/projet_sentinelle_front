190bf519b49978f71ceadf334b89d8a906608d3a
Ibikivan <82176594+Ibikivan@users.noreply.github.com>
2025-05-27 22:53:34 +0200

    Test

--- src/app/axios/axiosInstance.js
+++ src/app/axios/axiosInstance.js
@@ -2,6 +2,8 @@ import axios from "axios"
 
 const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 
+console.log("Axios base URL:", baseURL)
+
 const axiosInstance = axios.create({
     baseURL: baseURL,
     withCredentials: true,
